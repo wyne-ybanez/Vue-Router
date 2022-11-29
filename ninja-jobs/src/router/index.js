2459d9e5362cdf6729b186b3e417e81b919d98fb
wyne-ybanez <wyne_01@icloud.com>
2022-11-29 16:07:00 +0000

    Update: implement lazy loading sample

--- ninja-jobs/src/router/index.js
+++ ninja-jobs/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-import About from '../views/About.vue'
+// import About from '../views/About.vue'
 import NotFound from '../views/NotFound.vue'
 import Jobs from '../views/jobs/Jobs.vue'
 import JobDetails from "../views/jobs/JobDetails.vue"
@@ -14,15 +14,16 @@ const routes = [
   {
     path: "/about",
     name: "About",
-    component: About,
     /**
-     * EXPLANATION:
+     * LAZY LOADING - EXPLANATION:
+     *
      * route level code-splitting
      * this generates a separate chunk (about.[hash].js) for this route
-     * which is lazy-loaded when the route is visited.
+     * which is lazy-loaded when the route is visited = Reducing loading time.
      *
-     * component: () => import(/* webpackChunkName: "about"... '../views/About.vue')
+     * component: () => import(/* webpackChunkName: "about"... ' ../views/About.vue')
      */
+    component: () => import('../views/About.vue')
   },
   {
     path: "/jobs/",
